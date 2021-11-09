e72d8b2f2f6ba61a249c209a28eee7e558dfb01a
ppesterev <ppesterev@mail.ru>
2021-11-09 21:18:10 +0700

    clean up debris from styled-components experiment

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,11 +2,8 @@ import { render } from "preact";
 
 import App from "./components/App";
 
-import { GlobalStyle } from "./styled";
-
 render(
   <>
-    <GlobalStyle />
     <App />
   </>,
   document.getElementById("app")!
