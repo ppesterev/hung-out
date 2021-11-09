1623c6b2d6d240f8dfef4061b82f97f6b1895f80
ppesterev <ppesterev@mail.ru>
2021-11-09 21:28:26 +0700

    basic test of websocket functionality

--- src/client/components/WelcomeScreen/index.tsx
+++ src/client/components/WelcomeScreen/index.tsx
@@ -1,14 +1,41 @@
+import { useState } from "preact/hooks";
+
 import "./style.css";
 
+const WS_URL =
+  process.env.NODE_ENV === "production"
+    ? `wss://${window.location.host}`
+    : `ws://localhost:9001`;
+
 export default function WelcomeScreen() {
+  const [username, setUsername] = useState("");
+  const [message, setMessage] = useState<string | null>(null);
+
   return (
     <div className="welcome-screen">
-      <form className="join-form">
+      <form
+        className="join-form"
+        onSubmit={(evt) => {
+          evt.preventDefault();
+          const url = new URL(`/?username=${username}`, WS_URL);
+          const ws = new WebSocket(url);
+          ws.addEventListener("message", (evt) => {
+            setMessage(evt.data);
+          });
+        }}
+      >
         <label>
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onInput={(evt) => setUsername(evt.currentTarget.value)}
+          />
           <button type="submit">Join game</button>
         </label>
       </form>
+
+      {message && <p>{message}</p>}
     </div>
   );
 }
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,46 @@
 import { createServer } from "http";
+import { WebSocket, WebSocketServer } from "ws";
 
 import serveStaticFiles from "./serve-static-files";
+
+const userConnections: {
+  [key: string]: { username: string; connection: WebSocket };
+} = {};
 
 const PORT = process.env.PORT || 9001;
 
 const server = createServer((req, res) => {
   return serveStaticFiles(req, res);
+});
+
+const wsServer = new WebSocketServer({ server });
+wsServer.on("connection", (ws, req) => {
+  const reqUrl = new URL(req.url!, `wss://${req.headers.host}`);
+  console.log(reqUrl);
+
+  const username = reqUrl.searchParams.get("username");
+
+  let error = null;
+  if (!username) {
+    error = "Connection request must include a username";
+    ws.send(JSON.stringify({ error }));
+    ws.close();
+    return;
+  }
+
+  if (username in userConnections) {
+    error = "Username is taken";
+    ws.send(JSON.stringify({ error }));
+    ws.close();
+    return;
+  }
+
+  userConnections[username] = {
+    username,
+    connection: ws
+  };
+
+  ws.send(JSON.stringify({ data: `Hello, ${username}` }));
 });
 
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
