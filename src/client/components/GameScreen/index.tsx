5ed568acc64f9ee95e72388e01e45881cb1b5f34
ppesterev <ppesterev@mail.ru>
2021-11-17 23:34:25 +0700

    game screen container layout

--- src/client/components/GameScreen/index.tsx
+++ src/client/components/GameScreen/index.tsx
@@ -4,6 +4,8 @@ import { GameState } from "../../../shared/types";
 import { Message } from "../../types";
 import * as api from "../../api";
 
+import "./style.css";
+
 interface Props {
   users: string[];
   messages: Message[];
@@ -20,40 +22,49 @@ export default function GameScreen({
   const [messageText, setMessageText] = useState("");
   return (
     <div className="game-screen">
-      <h2>Users</h2>
-      <ul>
-        {users.map((user) => (
-          <li>{user}</li>
-        ))}
-      </ul>
-      <h2>Chat</h2>
-      <ul>
-        {messages.map((msg) => (
-          <li>
-            {msg.username === null ? msg.text : `${msg.username}: ${msg.text}`}
-          </li>
-        ))}
-      </ul>
-      <h2>Game state</h2>
-      <ul>
-        <li>Term: {gameState.partialTerm}</li>
-        <li>Mistakes: {gameState.mistakes}</li>
-      </ul>
-      <form
-        className="chat-form"
-        onSubmit={(evt) => {
-          evt.preventDefault();
-          api.sendMessage({ text: messageText, isGuess: true });
-        }}
-      >
-        <input
-          type="text"
-          name="message"
-          value={messageText}
-          onInput={(evt) => setMessageText(evt.currentTarget.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
+      <section className="user-list">
+        <h2>Users</h2>
+        <ul>
+          {users.map((user) => (
+            <li>{user}</li>
+          ))}
+        </ul>
+      </section>
+      <section className="chat">
+        <h2>Chat</h2>
+        <ul>
+          {messages.map((msg) => (
+            <li>
+              {msg.username === null
+                ? msg.text
+                : `${msg.username}: ${msg.text}`}
+            </li>
+          ))}
+        </ul>
+        <form
+          className="chat-form"
+          onSubmit={(evt) => {
+            evt.preventDefault();
+            api.sendMessage({ text: messageText, isGuess: true });
+          }}
+        >
+          <input
+            type="text"
+            name="message"
+            value={messageText}
+            onInput={(evt) => setMessageText(evt.currentTarget.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </section>
+      <section className="game-state">
+        <h2>Game state</h2>
+        <ul>
+          <li>Term: {gameState.partialTerm}</li>
+          <li>Mistakes: {gameState.mistakes}</li>
+        </ul>
+      </section>
+
       <button
         type="button"
         onClick={() => {
