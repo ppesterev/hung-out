19ad9d32d620374afcaf4a5a20aa83ff06fcc1a1
ppesterev <ppesterev@mail.ru>
2021-11-21 00:33:46 +0700

    use chat component; add block prefixes to game screen element classes

--- src/client/components/GameScreen/index.tsx
+++ src/client/components/GameScreen/index.tsx
@@ -1,10 +1,9 @@
-import { useState } from "preact/hooks";
-
 import { GameState } from "../../../shared/types";
 import { Message } from "../../types";
 import * as api from "../../api";
 
 import HangmanGraphic from "../HangmanGraphic";
+import Chat from "../Chat";
 
 import "./style.css";
 
@@ -21,11 +20,10 @@ export default function GameScreen({
   gameState,
   onDisconnected
 }: Props) {
-  const [messageText, setMessageText] = useState("");
   return (
     <div className="game-screen">
-      <span className="term">{gameState.partialTerm}</span>
-      <section className="user-list">
+      <span className="game-screen__term">{gameState.partialTerm}</span>
+      <section className="game-screen__user-list">
         <h2>Users</h2>
         <ul>
           {users.map((user) => (
@@ -33,41 +31,17 @@ export default function GameScreen({
           ))}
         </ul>
       </section>
-      <section className="chat">
-        <h2>Chat</h2>
-        <ul>
-          {messages.map((msg) => (
-            <li>
-              {msg.username === null
-                ? msg.text
-                : `${msg.username}: ${msg.text}`}
-            </li>
-          ))}
-        </ul>
-        <form
-          className="chat-form"
-          onSubmit={(evt) => {
-            evt.preventDefault();
-            api.sendMessage({ text: messageText, isGuess: true });
-          }}
-        >
-          <input
-            type="text"
-            name="message"
-            value={messageText}
-            onInput={(evt) => setMessageText(evt.currentTarget.value)}
-          />
-          <button type="submit">Send</button>
-        </form>
+      <section className="game-screen__chat">
+        <Chat messages={messages} />
       </section>
-      <section className="game-state">
+      <section className="game-screen__game-state">
         <h2>Game state</h2>
         <span>Mistakes: {gameState.mistakes}</span>
         <HangmanGraphic mistakeCount={gameState.mistakes?.length || 0} />
       </section>
 
       <button
-        class="leave-btn"
+        class="game-screen__leave-btn"
         type="button"
         onClick={() => {
           api.disconnect();
